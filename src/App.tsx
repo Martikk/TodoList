4a654bd4042af0658e0c4556745eab096fa99e9b
your_name <alex.martishyn@gmail.com>
2024-07-27 17:02:22 -0700

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import './App.css';
-import {Todolist} from "./Todolist";
+import {TaskType, Todolist} from "./Todolist";
+
 
 function App() {
+
+    let tasks1: Array<TaskType> =[
+        {id:1,title:"CSS", isDone:true},
+        {id:2,title:"JS", isDone:true},
+        {id:3,title:"React", isDone:false},
+    ]
+
+    let tasks2: Array<TaskType> =[
+        {id:1,title:"Terminator", isDone:true},
+        {id:2,title:"XXX", isDone:false},
+        {id:3,title:"Jentelmens", isDone:true},
+    ]
+
   return (
       <div className="App">
-          <Todolist title="What to learn"/>
-          <Todolist title="Movies"/>
-        <input type="checkbox"/>
-        <input type="date"/>
-        <input placeholder="placeholder"/>
+          <Todolist title="What to learn" tasks={tasks1}/>
+          <Todolist title="Movies" tasks={tasks2}/>
       </div>
   );
 }
